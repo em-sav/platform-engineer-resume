48d35e659842f6f90ddcc9f90ae7078137650d4f
ÃÉmile Savard <emile.savard.21@gmail.com>
2023-01-20 13:53:49 -0500

    fix: png images

--- src/global/config.ts
+++ src/global/config.ts
@@ -1,4 +1,5 @@
 export const originPath = () => {
+    // return "https://emtheskills.github.io/software-engineer-resume/"
     if (process.env.STAGE_ENV !== "PROD") {
         return window.location.origin
     } else {
