3280bab5d06861a1e04c47920969a0ac33973f25
ÃÉmile Savard <emile.savard.21@gmail.com>
2023-01-20 12:52:07 -0500

    ops: trying prod build

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,22 @@
-/**
- * @license
- * Copyright 2018 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */
+import merge from 'deepmerge';
+import { createSpaConfig } from '@open-wc/building-rollup';
+import copy from "rollup-plugin-copy-assets";
 
-import html from '@web/rollup-plugin-html';
-import {copy} from '@web/rollup-plugin-copy';
-import resolve from '@rollup/plugin-node-resolve';
-import {terser} from 'rollup-plugin-terser';
-import minifyHTML from 'rollup-plugin-minify-html-literals';
-import summary from 'rollup-plugin-summary';
+const baseConfig = createSpaConfig({
+  developmentMode: process.env.ROLLUP_WATCH === 'true',
+  injectServiceWorker: false
+});
 
-export default {
+export default merge(baseConfig, {
+  input: './index.html',
+  output: {
+    dir: './docs'
+  },
   plugins: [
-    // Entry point for application build; can specify a glob to build multiple
-    // HTML files for non-SPA app
-    html({
-      input: 'index.html',
-    }),
-    // Resolve bare module specifiers to relative paths
-    resolve(),
-    // Minify HTML template literals
-    minifyHTML(),
-    // Minify JS
-    terser({
-      ecma: 2020,
-      module: true,
-      warnings: true,
-    }),
-    // Print bundle summary
-    summary(),
-    // Optional: copy any static assets to build directory
     copy({
-      patterns: ['assets/**/*'],
+      assets: [
+        "../../assets"
+      ],
     }),
   ],
-  output: {
-    dir: 'docs',
-  },
-  preserveEntrySignatures: 'strict',
-};
+});
--- target/src/r-app.js
+++ target/src/r-app.js
@@ -1,3 +1,9 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 import { css, html, LitElement } from "lit";
 import { customElement } from "lit/decorators.js";
 import "./components/title/r-title";
@@ -10,10 +16,32 @@ import "./components/awards/r-awards";
 import "./components/implications/r-implications";
 import "./components/skills/r-skills";
 import { typography } from "./global/theme";
-
-@customElement("r-app")
-export class RApp extends LitElement {
-  static override styles = css`
+let RApp = class RApp extends LitElement {
+    render() {
+        return html `
+      <div class="container">
+        <r-title></r-title>
+        <div class="split-container">
+          <r-contact class="middle-spacing responsive"></r-contact>
+          <r-summary class="responsive"></r-summary>
+        </div>
+        <div class="split-container">
+          <r-education class="middle-spacing responsive"></r-education>
+          <r-experience class="responsive reduce-gap"></r-experience>
+        </div>
+        <div class="split-container">
+          <r-skills class="middle-spacing responsive fit-column-skills"></r-skills>
+          <r-awards class="responsive reduce-gap fit-column-awards"></r-awards>
+        </div>
+        <div class="split-container" style>
+          <r-implications class="middle-spacing responsive reduce-gap fit-column-implications"></r-implications>
+          <div class="responsive copyrights">Copyright © 2023 Émile Savard</div>
+        </div>
+      </div>
+    `;
+    }
+};
+RApp.styles = css `
     :host {
       font-family: Montserrat, sans-serif;
     }
@@ -93,34 +121,8 @@ export class RApp extends LitElement {
       }
     }
   `;
-
-  override render() {
-    return html`
-      <div class="container">
-        <r-title></r-title>
-        <div class="split-container">
-          <r-contact class="middle-spacing responsive"></r-contact>
-          <r-summary class="responsive"></r-summary>
-        </div>
-        <div class="split-container">
-          <r-education class="middle-spacing responsive"></r-education>
-          <r-experience class="responsive reduce-gap"></r-experience>
-        </div>
-        <div class="split-container">
-          <r-skills class="middle-spacing responsive fit-column-skills"></r-skills>
-          <r-awards class="responsive reduce-gap fit-column-awards"></r-awards>
-        </div>
-        <div class="split-container" style>
-          <r-implications class="middle-spacing responsive reduce-gap fit-column-implications"></r-implications>
-          <div class="responsive copyrights">Copyright © 2023 Émile Savard</div>
-        </div>
-      </div>
-    `;
-  }
-}
-
-declare global {
-  interface HTMLElementTagNameMap {
-    "r-app": RApp;
-  }
-}
+RApp = __decorate([
+    customElement("r-app")
+], RApp);
+export { RApp };
+//# sourceMappingURL=r-app.js.map
