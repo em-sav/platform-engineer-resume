13e9daa5ad947ae3df20f693582aad3b1b80fd55
ÃÉmile Savard <emile.savard.21@gmail.com>
2023-01-20 14:01:29 -0500

    fix: assets loading path 3

--- src/global/config.ts
+++ src/global/config.ts
@@ -1,5 +1,5 @@
 export const originPath = () => {
-    return "https://emtheskills.github.io/software-engineer-resume/assets/beneva.png"
+    return "https://emtheskills.github.io/software-engineer-resume/"
     // if (process.env.STAGE_ENV !== "PROD") {
     //     return window.location.origin
     // } else {
