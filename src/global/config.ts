beb0e7874991a09266cff679041791e787c23725
ÃÉmile Savard <emile.savard.21@gmail.com>
2023-01-20 19:27:32 -0500

    fix: assets url path

--- src/global/config.ts
+++ src/global/config.ts
@@ -1,8 +1,3 @@
 export const originPath = () => {
-    // return "https://emtheskills.github.io/software-engineer-resume/"
-    if (process.env.STAGE_ENV !== "PROD") {
-        return window.location.origin
-    } else {
-       return window.location.href
-    }
+    return window.location.href;
 }
